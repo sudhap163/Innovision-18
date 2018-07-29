011d2db6390c7ca4e522a816edbcdfe126a3abc2
sudhap163 <priyadarshini.sudha3@gmail.com>
2018-07-29 12:28:11 +0530

    CA login added

--- Innovision/apis/panels/CASelection/displayResponses.php
+++ Innovision/apis/panels/CASelection/displayResponses.php
@@ -1,17 +1,21 @@
 <?php
 
 	include('../../db.php');
+    include('./verifyCAAdmin.php');
 
 	if($_SERVER["REQUEST_METHOD"] === "GET") {
 
-		$query = mysqli_query($conn, "SELECT * FROM ca_selection_responses");
+		if (isset($status) && $status === "success") {
 
-		$result = array();
-		while($res = mysqli_fetch_array($query, MYSQL_ASSOC)) {
-    		$result[] = $res;
+			$query = mysqli_query($conn, "SELECT * FROM ca_selection_responses");
+
+			$result = array();
+			while($res = mysqli_fetch_array($query, MYSQL_ASSOC)) {
+	    		$result[] = $res;
+			}
+
+			echo(json_encode(array('status' => 'success', 'result' => $result)));
 		}
-
-		echo(json_encode(array('status' => 'success', 'result' => $result)));
 	}
 
 ?>
