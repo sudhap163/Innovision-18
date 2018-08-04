0a8e8eac169a4242f72ade40d6ed526c5b854dd4
shaswat <shaswatlenka10@outlook.com>
2018-08-05 05:21:57 +0530

    change theme and overall look and feel

--- Innovision/js/ajax/event_individual.js
+++ Innovision/js/ajax/event_individual.js
@@ -11,7 +11,24 @@ $(document).ready(function(){
 				if(dataArray["status"] == "success")
 				{
 					var eventArr = dataArray["result"];
-					populate(eventArr);
+					/*$(".poster").children("div").html("<img height='200' width='200' class='img-responsive' src='"+eventArr["image_path"]+"'>");
+					event_desc = (eventArr["description"]).split("//");
+					event_rules = (eventArr["rules"]).split("//");
+					event_judging_criteria = (eventArr["judging_criteria"]).split("//");
+					for(var i=0 ; i<event_desc.length ; i++)
+					{
+						$(".desc").append("<p>"+event_desc[i]+"</p>");
+					}
+					for(var i=0 ; i<event_rules.length ; i++)
+					{
+						$(".rules").append("<p>"+event_rules[i]+"</p>");
+					}
+					for(var i=0 ; i<event_judging_criteria.length ; i++)
+					{
+						$(".criteria").append("<p>"+event_judging_criteria[i]+"</p>");
+					}
+					$(".details").append("<p>Date of Event: "+eventArr["date"]+"</p><p>Time of Event: "+eventArr["time"]+"</p><p>Venue: "+eventArr["venue"]+"</p>");
+					$(".details_coordinator").append("<h2>Coordinator 1</h2><p>Name: "+eventArr["coordinatorName1"]+"</p><p>Contact: "+eventArr["coordinatorContact1"]+"</p><h2>Coordinator 2</h2><p>Name: "+eventArr["coordinatorName2"]+"</p><p>Contact: "+eventArr["coordinatorContact2"]+"</p>");*/
 				}
 			}
 		});
