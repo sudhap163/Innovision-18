739dfe1bccdef2dd065c9b31ccded1e436ba4a4c
Ajit Kumar Behera <ajit456behera@gmail.com>
2018-10-24 10:15:16 +0530

    Feedback integration

--- Innovision/js/ajax/submit_feedback.js
+++ Innovision/js/ajax/submit_feedback.js
@@ -1,11 +1,36 @@
 $(document).on("click", ".submit_feedback", function(){
-    //var inno_id = localStorage.inno_id;
-    //var name = localStorage.name;
-    //var college = localStorage.college;
-    var liked = $("#liked_event").val();
-    var disliked = $("#disliked_event").val();
-    var ratings = $("#ratings").val();
-    var suggestions = $("#suggest_improve").val();
+    // var _id = localStorage.inno_id;
+    // var Name = localStorage.name;
+    // var clg = localStorage.college;
+    var tkn = localStorage.token;
+    var liked_event = $("#liked_event").val();
+    var disliked_event = $("#disliked_event").val();
+    var rtg = $("#ratings").val();
+    var sg = $("#suggest_improve").val();
     var last = $(":checked").val();
-    console.log(last);
+    $.ajax({
+        type: "POST",
+        url: "./apis/participantRegistration/feedback.php",
+        data: {
+            token : tkn,
+            liked : liked_event,
+            disliked : disliked_event,
+            rating : rtg,
+            suggestion : sg,
+            next_yr : last
+        },
+        success : function(data){
+            if(JSON.parse(data).status == "success")
+            {
+                swal("Success!", JSON.parse(data).message , "success");
+            }
+            else
+            {
+                swal("Sorry!", JSON.parse(data).message, "failure");
+            }
+        },
+        error: function(data){
+            console.log("Couldn't process request");
+        }
+    });
 });
